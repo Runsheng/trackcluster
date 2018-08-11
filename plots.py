c27955664c741b93c5e812f89c6f2a4e71884d08
runsheng <runsheng.lee@gmail.com>
2018-08-11 17:08:17 +0800

    test plot

--- plots.py
+++ plots.py
@@ -14,15 +14,18 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import scipy
 import pylab
+import numpy as np
 import scipy.cluster.hierarchy as sch
 from cluster import cal_distance
+from copy import deepcopy
+
 
 def line_plot(bigg_list, color=None, out="test.pdf"):
     """
-
     :param bigg_list: a list contain multiple bigGenePred class
     :return:
     """
+    bigg_list_bk=deepcopy(bigg_list)
 
     D=cal_distance(bigg_list)
 
@@ -32,60 +35,64 @@ def line_plot(bigg_list, color=None, out="test.pdf"):
     start=i.chromStart
     i.get_exon(start)
     width=(i.chromEnd-start)/400.0 # 100 bp=1 inch width
-    height=(len(bigg_list))/5.0+2
+    height=(len(bigg_list))/5.0+2  # 1 gene 0.2 inch height
     fig=plt.figure(figsize=(width,height))
 
     print(width, height)
 
     #### first the dendrogram
-    ax1 = fig.add_axes([0.02, 0.09, 0.2, 0.85]) # set region
+    ax1 = fig.add_axes([0.02, 0.09, 0.2, 0.91]) # set region
 
-    Y = sch.linkage(D, method='average')
+    Y = sch.linkage(D, method='single')
     Z1 = sch.dendrogram(Y, orientation='left')
 
+    def flatten(l):
+        return [item for sublist in l for item in sublist]
+
+    x_list = flatten(Z1['icoord'])
+    y_list = flatten(Z1['dcoord'])
+
+    color_xy_list = []
+    for i in Z1["color_list"]:
+        color_xy_list.extend(i * 4)
+
+    leave_cords = [x for x,y in zip(x_list, y_list) if y == 0]
+    leave_color=[c for y, c in zip(y_list, color_xy_list) if y==0]
+
+    # in the dendogram data structure,
+    # leave ids are listed in ascending order according to their x-coordinate
+    order = np.argsort([x for x in leave_cords])
+    id_cord = [leave_cords[idx] for idx in order]  # <- main data structure
+    id_color = [leave_color[idx] for idx in order]
+
+    ymax=max(id_cord)
+
+    #id_to_coord = dict(zip(Z1['leaves'], [leave_coords[idx] for idx in order]))
+
     #ax1.set_xticks([])
-    ax1.set_yticks([])
+    #ax1.set_yticks([])
 
     # second, the track for gene models
 
     # get the order of the gene models in dendrogram
     bigg_list_new=[]
     for x in Z1["leaves"]:
-        bigg_list_new.append(bigg_list[x])
+        bigg_list_new.append(bigg_list_bk[x])
 
-    # get the real colour for each leaves (need to +1)
-    color_l=[]
-    dcorrd_l=[] # unused after update
-    for n, line in enumerate(zip(Z1["color_list"], Z1["icoord"])):
-        x, y=line
-        if n==0:
-            pass
-        else:
-            if x==(Z1['color_list'])[n-1]:
-                pass
-            else:
-                color_l.append((Z1['color_list'])[n-1])
-                dcorrd_l.append(Z1["icoord"][n-1][1])
-        color_l.append(x)
-        dcorrd_l.append(y[1])
-
-
-    print(len(bigg_list_new), len(color_l), len(dcorrd_l))
-
-    ax2=fig.add_axes([0.25,0.09,0.65,0.85]) # adjust the ax to fit figure
+    ax2=fig.add_axes([0.25,0.09,0.65,0.91]) # adjust the ax to fit figure
     for n,i in enumerate(bigg_list_new):
         i.get_exon(start)
         for exon in i.exon:
             x_start, x_end = exon
             # debug
             #print n, x_start, x_end
-            l=plt.hlines(y=n+0.8,  xmin=x_start, xmax=x_end, linewidth=4, colors=color_l[n])
+            plt.hlines(y=id_cord[n],  xmin=x_start, xmax=x_end, linewidth=4, colors=id_color[n])
 
         for intron in i.intron:
             x_start, x_end = intron
-            l=plt.hlines(y=n+0.8,  xmin=x_start, xmax=x_end, linewidth=1, colors=color_l[n])
+            plt.hlines(y=id_cord[n],  xmin=x_start, xmax=x_end, linewidth=1, colors=id_color[n], linestyles="dotted")
 
-
-    ax2.set_yticks([])
+    plt.ylim(0, ymax)
+    #ax2.set_yticks([])
     plt.savefig(out)
     #plt.show()
